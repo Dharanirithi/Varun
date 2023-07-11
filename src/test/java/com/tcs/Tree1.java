5c15345f5db5d88cc1852a8740adf6c996ac1584
Dharanirithi <dharanirithi98@gmail.com>
2023-07-11 18:55:16 +0530

    master push

---
+++ src/test/java/com/tcs/Tree1.java
@@ -0,0 +1,15 @@
+package com.tcs;
+
+public class Tree1 {
+	public void one() {
+		System.out.println("one");
+	}
+
+	public void two() {
+		System.out.println("two");
+	}
+
+	public void three() {
+		System.out.println("three");
+	}
+}
